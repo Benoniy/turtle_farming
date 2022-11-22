5333182ea69020a74583c8fa9d28a2e9d5b44b74
Benjamin Ranson <47002663+Benoniy@users.noreply.github.com>
2022-11-22 19:18:41 +0000

    Create monitor_farm.lua

---
+++ monitor_farm.lua
@@ -0,0 +1,10 @@
+local monitor = peripheral.find("monitor")
+local modem = peripheral.find("modem")
+modem.open(15)
+
+
+local event, side, channel, replyChannel, message, distance
+while true do
+    event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
+    monitor.write(message)
+end
