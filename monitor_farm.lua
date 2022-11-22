b04c62e5c76616a39cb248b6e57f39ea881dd534
Benjamin Ranson <47002663+Benoniy@users.noreply.github.com>
2022-11-22 20:14:28 +0000

    Update monitor_farm.lua

--- monitor_farm.lua
+++ monitor_farm.lua
@@ -1,10 +1,18 @@
 local monitor = peripheral.find("monitor")
 local modem = peripheral.find("modem")
+local curs_y = 1
 modem.open(15)
 
+
 monitor.clear()
+montior.setCurorPos(1,1)
+
+
 local event, side, channel, replyChannel, message, distance
 while true do
     event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
     monitor.write(message .. "\n")
+    
+    curs_y = curs_y + 1
+    montior.setCurorPos(1,curs_y)
 end
